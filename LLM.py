a449c6ab4d797a90be3eff0295c0b0f6a8d3bd51
Tobaisfire <kevalsaud25@gmail.com>
2023-06-24 15:00:00 +0530

    App ready

---
+++ LLM.py
@@ -0,0 +1,136 @@
+
+from langchain.embeddings import CohereEmbeddings
+import weaviate
+import json
+
+client = weaviate.Client(
+    url = "https://llm-chatai-bot-v2-mmelxkrr.weaviate.network",  # Replace with your endpoint
+    auth_client_secret=weaviate.AuthApiKey(api_key="RDdL12G2jw7mm8ns7DaDBRkdU2mT8OTAkGOG"),  # Replace w/ your Weaviate instance API key
+    additional_headers = {
+       "X-Cohere-Api-Key": "PoQqB6c283yGmex4A2cSwQWxYj5oP1rh9bkuqKYy"  # Replace with your inference API key
+    }
+)
+embeddings = CohereEmbeddings(cohere_api_key='PoQqB6c283yGmex4A2cSwQWxYj5oP1rh9bkuqKYy',model='multilingual-22-12')
+
+
+
+
+from langchain.callbacks import get_openai_callback
+
+def count_tokens(agent, query):
+    with get_openai_callback() as cb:
+        result = agent.run(query)
+        print(f'Spent a total of {cb.total_tokens} tokens')
+
+    return result
+
+
+from pymongo import MongoClient
+class Mem:
+    def __init__(self) -> None:
+        pass
+    def bot_memory(self,id_session):
+        client = MongoClient('mongodb://localhost:27017/')
+        
+        db = client['Bot-history']
+
+        collection = db['Chat-history']
+
+        existing_data = collection.find_one({'Id': id_session})
+        if existing_data!=None:
+            
+            return eval(collection.find_one({'Id': id_session})['msg_history'])
+
+        else:
+            data ={ 'Id' :id_session,
+            'msg_history':str([])}
+
+            self.insert_or_update_data(data)
+
+            return eval(collection.find_one({'Id': id_session})['msg_history'])
+            
+
+
+    def insert_or_update_data(self,data):
+  
+        client = MongoClient('mongodb://localhost:27017/')
+    
+        try:
+          
+            db = client['Bot-history']
+    
+      
+            collection = db['Chat-history']
+  
+            existing_data = collection.find_one({'Id': data['Id']})
+    
+            if existing_data!=None:
+    
+         
+
+                exist_list = eval(existing_data['msg_history'])
+                new_msg_history = exist_list + data['msg_history']
+    
+         
+                collection.update_one({'Id': data['Id']}, {'$set': {'msg_history': str(new_msg_history)}})
+   
+            else:
+
+                collection.insert_one(data)
+ 
+        except Exception as e:
+            print('Error while inserting/updating data:', e)
+        finally:
+     
+            client.close()
+
+
+
+
+
+from langchain.vectorstores.weaviate import Weaviate
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ChatVectorDBChain
+
+
+
+PROMPT = PromptTemplate(
+
+    template="""Your are a helpful assitant, Use the following pieces of context to answer the specific question \
+                remaining you will answer from your chat_history \
+                at the end. If you don't know the answer, just say that you don't know, \
+                don't try to make up an answer. Note you are an assitant created by keval.
+
+                {context}
+
+                {chat_history}             
+                Question: {question}
+                
+          """,
+    input_variables=["chat_history","question","context"]
+)
+
+chain_type_kwargs = {"prompt": PROMPT}
+
+
+client = weaviate.Client(
+    url = "https://llm-chatai-bot-v2-mmelxkrr.weaviate.network",  # Replace with your endpoint
+    auth_client_secret=weaviate.AuthApiKey(api_key="RDdL12G2jw7mm8ns7DaDBRkdU2mT8OTAkGOG"),  # Replace w/ your Weaviate instance API key
+    additional_headers = {
+       "X-Cohere-Api-Key": "PoQqB6c283yGmex4A2cSwQWxYj5oP1rh9bkuqKYy"  # Replace with your inference API key
+    })
+
+vectorstore = Weaviate(client, "Wikipedia", "text")
+
+
+qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(openai_api_key='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',temperature=0,model='gpt-3.5-turbo'),
+                                           vectorstore.as_retriever(),
+                                        #    memory=memory,
+                                           combine_docs_chain_kwargs=chain_type_kwargs,
+                                          #  verbose= True
+                                           )
+
+
+
---
+++ app.py
@@ -0,0 +1,70 @@
+
+from flask import Flask, request, jsonify,render_template,redirect,session
+from time import sleep
+from LLM import *
+app = Flask(__name__)
+
+app.secret_key = 'your_secret_key'
+memory = Mem()
+
+@app.route('/',methods= ['POST','GET'])
+
+def id_get():
+    name = request.form.get('name')
+
+    if name !=None:
+        session['user'] = name
+        return redirect('/Chat')
+   
+    return render_template('id_fecther.html')
+
+
+@app.route('/Chat')
+def home():
+    if session['user'] != None:
+        
+   
+        return render_template('UI.html')
+
+    return redirect('/')
+
+
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user = session['user']
+    print(user)
+    data = request.get_json()
+    message = data['message']
+    
+    print(message)
+
+    bot_reply = generate_reply(message,user)
+    
+    response = {
+        'reply': bot_reply
+    }
+    
+    return jsonify(response)
+
+
+
+
+def generate_reply(message,user_id):
+    chat_history = memory.bot_memory(user_id)
+
+    query = message
+
+    result = qa({"question": str(query),"chat_history":chat_history })
+
+    sleep(3)
+    chat_history = [(query, result["answer"])]
+
+    data = {'Id':user_id,'msg_history':chat_history}
+
+    memory.insert_or_update_data(data)
+  
+    return result["answer"]
+
+if __name__ == '__main__':
+    app.run()
